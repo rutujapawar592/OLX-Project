14830c88660c6751edf315f5d76b8c9d2b017dc2
rutujapawar592 <57221968+rutujapawar592@users.noreply.github.com>
2022-04-25 19:52:57 +0530

    Update MasterServiceImpl.java

--- OLX-MasterData/src/main/java/com/olx/masterdata/service/MasterServiceImpl.java
+++ OLX-MasterData/src/main/java/com/olx/masterdata/service/MasterServiceImpl.java
@@ -63,40 +63,24 @@ public class MasterServiceImpl implements MasterService {
 	
 	
 	private Status convertEntityIDTO(StatusEntity statusEntity) {
-		//return new StatusEntity(Status.getId(), Status.getName(), Status.getMarket(), Status.getPrice());
-//		TypeMap<StatusEntity, Status> typeMap = modelMapper.typeMap(StatusEntity.class, Status.class);
-//		typeMap.addMappings(mapper -> {
-//			mapper.map(StatusEntity::getma, Status::setMarket);
-//		});
+		
 		Status status = modelMapper.map(statusEntity, Status.class);
 		return status;
 	}
 	private StatusEntity convertDTOIntoEntity(Status status) {
-		
-//		TypeMap<Status, StatusEntity> typeMap = modelMapper.typeMap(Status.class, StatusEntity.class);
-//		typeMap.addMappings(mapper -> {
-//			mapper.map(Status::getMarket, StatusEntity::setmarketName);
-//		});
+
 		StatusEntity statusEntity = modelMapper.map(status, StatusEntity.class);
 		
 		return statusEntity;
 	}
 	
 	private Category convertEntityInDTO(CategoryEntity categoryEntity) {
-		//return new StatusEntity(Status.getId(), Status.getName(), Status.getMarket(), Status.getPrice());
-//		TypeMap<StatusEntity, Status> typeMap = modelMapper.typeMap(StatusEntity.class, Status.class);
-//		typeMap.addMappings(mapper -> {
-//			mapper.map(StatusEntity::getma, Status::setMarket);
-//		});
+		
 		Category category = modelMapper.map(categoryEntity, Category.class);
 		return category;
 	}
 	private CategoryEntity convertDTOInEntity(Category category) {
-		
-//		TypeMap<Status, StatusEntity> typeMap = modelMapper.typeMap(Status.class, StatusEntity.class);
-//		typeMap.addMappings(mapper -> {
-//			mapper.map(Status::getMarket, StatusEntity::setmarketName);
-//		});
+
 		CategoryEntity categoryEntity = modelMapper.map(category, CategoryEntity.class);
 		
 		return categoryEntity;
